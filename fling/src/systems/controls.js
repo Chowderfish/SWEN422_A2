fb54a8a62acb56b2680ba348318f55aab9c16f64
Andrew Hay <hayandr1@ecs.vuw.ac.nz>
2019-10-05 17:49:59 +1300

    Reimplemented touch input in attempt to tame swipes. Mixed results

--- fling/src/systems/controls.js
+++ fling/src/systems/controls.js
@@ -15,58 +15,30 @@ export default (entities, { events }) => {
     let player = entities.player;
     let platforms = filter(entities, "platform");
 
-    let swipeUp = events.find(e => e.type === "swipe-up");
-    let swipeDown = events.find(e => e.type === "swipe-down");
-    let swipeLeft = events.find(e => e.type === "swipe-left");
-    let swipeRight = events.find(e => e.type === "swipe-right");
-    let tap = any(events, "type", "tap");
-    let hold = any(events, "type", "hold");
+    let swipe = events.find(e => e.type === "swipe");
 
     let grounded = find(platforms, p => standing(p, player));
 
     player.controls.gestures = {
-        swipeUp,
-        swipeDown,
-        swipeLeft,
-        swipeRight,
-        tap,
-        hold
+        swipe
     };
 
     let modes = [
         {
-            if: grounded && swipeLeft,
+            if: grounded && swipe,
             then: () => {
                 player.controls.mode = "platform";
-                player.direction.horizontal = "left";
-                player.body.force = swipeLeft.vector;
-                player.controls.gestures = {};
-            }
-        },
-        {
-            if: grounded && swipeRight,
-            then: () => {
-                player.controls.mode = "platform";
-                player.direction.horizontal = "right";
-                player.body.force = swipeRight.vector;
-                player.controls.gestures = {};
-            }
-        },
-        {
-            if: grounded && swipeUp,
-            then: () => {
-                player.controls.mode = "platform";
-                player.direction.vertical = "up";
-                player.body.force = swipeUp.vector;
-                player.controls.gestures = {};
-            }
-        },
-        {
-            if: grounded && swipeDown,
-            then: () => {
-                player.controls.mode = "platform";
-                player.direction.vertical = "down";
-                player.body.force = swipeDown.vector;
+                if (swipe.vector.x > 0) {
+                    player.direction.horizontal = "right";
+                } else if (swipe.vector.x < 0) {
+                    player.direction.horizontal = "left";
+                }
+                if (swipe.vector.y > 0) {
+                    player.direction.vertical = "down";
+                } else if (swipe.vector.y < 0) {
+                    player.direction.vertical = "up"
+                }
+                player.body.force = swipe.vector;
                 player.controls.gestures = {};
             }
         },
--- fling/src/systems/input.js
+++ fling/src/systems/input.js
@@ -1,48 +1,65 @@
 import { any } from "../utils";
 
+let verbose = false; //Exposes touch events to console for debugging
+
+let swipeStarted;
+let swipeEnded;
+let lastMoveEvent;
+
 const swipe = (touches, dispatch) => {
-    let move = touches.find(x => x.type === "move");
+    let move = touches.find(x => (x.type === "move" || x.type === "long-press"));
+    let fingerUp = any(touches, "type", "end");
+    let fingerDown = any(touches, "type", ["long-press", "move"]);
 
     if (move) {
-
-        let move_vector = { 
-            x: (move.delta.locationX * 100 / move.delta.timestamp),
-            y: (move.delta.locationY * 100 / move.delta.timestamp) };
-
-        if (move.delta.locationX < -2)
-            dispatch({ type: "swipe-left", vector: move_vector });
-
-        if (move.delta.locationX > 2)
-            dispatch({ type: "swipe-right", vector: move_vector });
-
-        if (move.delta.locationY < -2)
-            dispatch({ type: "swipe-up", vector: move_vector });
-
-        if (move.delta.locationY > 2)
-            dispatch({ type: "swipe-down", vector: move_vector });
+        if (verbose) {
+            console.log("Touch move event found");
+            if (swipeStarted === undefined) console.log("No swipe start event registered");
+            if (swipeEnded === undefined) console.log("No swipe end event registered");
+        }
+        lastMoveEvent = move;
     }
+
+    if (lastMoveEvent) {
+        if (swipeStarted) {
+            if (fingerUp || !fingerDown) {
+                if (verbose) {
+                    console.log("Finger up event found");
+                    console.log("Touch move event registered as swipe end");
+                }
+                swipeEnded = { x: lastMoveEvent.event.locationX, y: lastMoveEvent.event.locationY };
+            }
+        } else {
+            if (verbose) {
+                console.log("Touch move event registered as swipe start");
+            }
+            swipeStarted = { x: lastMoveEvent.event.locationX, y: lastMoveEvent.event.locationY };
+        }
+
+        if (swipeEnded) {
+            let deltaX = (swipeEnded.x - swipeStarted.x) / 5;
+            let deltaY = (swipeEnded.y - swipeStarted.y) / 10;
+
+            if (verbose) {
+                console.log("Dispatching swipe event");
+                console.log("dX = " + deltaX + " dY = " + deltaY);
+            }
+
+            let move_vector = { x: deltaX, y: deltaY };
+
+            swipeStarted = undefined;
+            swipeEnded = undefined;
+            lastMoveEvent = undefined;
+
+            dispatch({ type: "swipe", vector: move_vector });
+        }
+    }
+
 };
 
-const hold = (touches, events, dispatch) => {
-    let fingerDown = any(touches, "type", ["long-press", "move"]);
-    let fingerUp = any(touches, "type", "end");
-    let hold =  any(events, "type", "hold");
-
-    if ((fingerDown || hold) && !fingerUp)
-        dispatch({ type: "hold" });
-};
-
-const tap = (touches, dispatch) => {
-    let press = any(touches, "type", "press");
-
-    if (press)
-        dispatch({ type: "tap"})
-};
 
 export default (entities, { touches, events, dispatch }) => {
     swipe(touches, dispatch);
-    hold(touches, events, dispatch);
-    tap(touches, dispatch);
-
+    
     return entities;
 };
