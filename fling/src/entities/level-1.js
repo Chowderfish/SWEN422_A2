8795a37180c2fa5873572952de0d402d8c5ce785
Ollie <barroliv@myvuw.ac.nz>
2019-10-05 15:40:41 +1300

    Pushed properly?

--- fling/src/entities/level-1.js
+++ fling/src/entities/level-1.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { Dimensions } from "react-native";
 import Matter from "matter-js";
 import Platform from "../components/platform";
@@ -19,32 +18,60 @@ export default restart => {
 
     let engine = Matter.Engine.create({ enableSleeping: false });
     let world = engine.world;
-
     world.gravity = { x: 0, y: 2 };
+
+    let bottom = offsetY+500;
+    // plat_1: Platform(
+    //     world,
+    //     { x: cx-50, y: offsetY + 500  },
+    //     0,
+    //     platformWidth * 0.25
+    // )
+    let layers = [ //List of all possible layers. The ascii representation can be seen next to it
+        {first: {disposition: 0, width: 0.25}, second: null}, //                                 |  -  |
+        {first: {disposition: -50, width: 0.20}, second: {disposition: 50, width: 0.20}}//      |-   -|
+    ];
+
+    var gap = 100; //The gap between each layer
+    let level = [];
+    for(var i = 0; i < 10; i++) {
+        var random_platform = layers[Math.floor(Math.random() * layers.length).toString()];
+
+        var new_platform = Platform(
+            world,
+            {x: cx + first.disposition, t: bottom + ((i+1)*gap)},
+            0,
+            platformWidth * first.width
+        );
+
+        level[i] = new_platform;
+    }
+
+    
 
     return {
         physics: { engine: engine, world: world },
+        platform1: Platform( //Bottom floor
+            world,
+            { x: cx, y: bottom },
+            0,
+            platformWidth
+        ),
+        platform2: Platform( //Right wall. This needs bouncy mechanic
+            world,
+            { x: width, y: bottom},
+            1.5708,
+            platformWidth * 10
+        ),
+        platform3: Platform( //Left wall. This also needs bouncy mechanic
+            world,
+            { x: 0, y: bottom},
+            1.5708,
+            platformWidth * 10
+        ),
 
-        platform1: Platform(
-            world,
-            { x: cx-50, y: offsetY + 500  },
-            0,
-            platformWidth * 0.25
-        ),
-        platform2: Platform(
-            world,
-            { x: cx+50, y: offsetY + 350  },
-            0,
-            platformWidth * 0.25
-        ),
-        platform3: Platform(
-            world,
-            { x: cx, y: offsetY + 100  },
-            0,
-            platformWidth * 0.25
-        ),
 
-        player: Player(world, { x: cx, y: offsetY - 100  }),
+        player: Player(world, { x: cx, y: offsetY + 400  }),
 
         camera: { offsetY: 0 }
     };
